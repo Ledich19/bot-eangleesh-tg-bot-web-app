8c7bab6e74f775d3ad8728faa867336f81e08c0c
Aleksandr <ledich19@gmail.com>
2024-01-20 02:19:29 +0200

    feat: add structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
+import { Route, Routes } from "react-router-dom";
+import Game from "./components/Game/Game";
+import Winners from "./components/Winners/Winners";
 const tg = window.Telegram.WebApp;
 
 declare global {
@@ -24,6 +28,11 @@ function App() {
 
   return (
     <>
+    <Header/>
+    <Routes>
+      <Route index element={<Game/>}/>
+      <Route path="/winners" element={<Winners/>}/>
+    </Routes>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -41,9 +50,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
